90eaa651a4f45076ec248845246830c38bc476a4
Bryan Matsuo <bryan.matsuo@gmail.com>
2014-05-18 20:53:46 -0700

    add more test cases. corners seem to make sense.

--- rt_test.go
+++ rt_test.go
@@ -4,10 +4,15 @@ import "testing"
 
 func TestCompose(t *testing.T) {
 	for i, test := range []struct{ pat, param, path string }{
+		{"/", "", "/"},
+		{"/", "/", "//"},
 		{"/", "hello", "/hello"},
 		{"/hello", "world", "/hello"},
 		{"/hello/", "world", "/hello/world"},
-		{"/hello/", "", "/hello/"},
+		{"hello/", "world", "hello/world"},
+		// weird cases
+		{"", "", ""},
+		{"", "hello", ""},
 	} {
 		path := Compose(test.pat, test.param)
 		if path != test.path {
@@ -18,9 +23,14 @@ func TestCompose(t *testing.T) {
 
 func TestDecompose(t *testing.T) {
 	for i, test := range []struct{ pat, path, param string }{
+		{"/", "/", ""},
 		{"/", "/hello", "hello"},
 		{"/hello/", "/hello/world", "world"},
 		{"/hello", "/hello/world", ""},
+		// weird cases not generally seen
+		{"", "", ""},
+		{"", "hello", ""},
+		{"/", "", ""},
 	} {
 		param := Decompose(test.pat, test.path)
 		if param != test.param {
